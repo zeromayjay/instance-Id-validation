3f82c56a41af32f3f53f1c6be4b4478df8571602
zeromayjay <mai.hoang@hotmail.de>
2021-01-04 14:51:54 +0100

    fixed structure

--- rest-service-2/src/main/java/com/example/restservice2/RestServiceApplication2.java
+++ rest-service-2/src/main/java/com/example/restservice2/RestServiceApplication2.java
@@ -1,28 +1,16 @@
 package com.example.restservice2;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.discovery.EurekaClient;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
-@RestController
+@EnableAdminServer
 public class RestServiceApplication2 {
- 
-    @Autowired
-    @Lazy
-    private EurekaClient eurekaClient;
- 
-    @Value("${spring.application.name}")
-    private String appName;
- 
-    public static void main(String[] args) {
-        SpringApplication.run(RestServiceApplication2.class, args);
-    }
- 
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication2.class, args);
+	}
 
 }
